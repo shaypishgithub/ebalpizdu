20544a4f8eb23686afbef691f7f8f6362ea3733e
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-11 08:55:16 +0200

    Remove GrowGarden wrapper from script

--- GrowGarden.lua
+++ GrowGarden.lua
@@ -1,5 +1,4 @@
 return {
-    GrowGarden = {
         {"Natural Disaster Garfield", function()
             loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
         end},
@@ -7,4 +6,4 @@ return {
             loadstring(game:HttpGet("https://pastefy.app/k3c6jWio/raw", true))()
         end}
     }
-}
+
