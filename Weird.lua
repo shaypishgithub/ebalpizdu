bc5669ba0e4b38018ecbc577e266c29af3e95cf3
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-11 17:45:10 +0200

    Update Weird.lua

--- Weird.lua
+++ Weird.lua
@@ -8,19 +8,19 @@ return {
     {"⚡ Russian Soldier (MegaHack) - Weird Gun Game", function()
         loadstring(game:HttpGet("https://pastefy.app/AJPoCZcG/raw"))()
     end},
-    {"🎯 NXPhub/Swiftbara Hub (Версия 1)", function()
+    {"🎯 NXPhub/Swiftbara Hub", function()
    
         loadstring(game:HttpGet("https://pastefy.app/HxgKxr4K/raw"))()
     end},
-    {"🎯 NXPhub/Swiftbara Hub (Версия 2 - Silent Aim)", function()
+    {"🎯 NXPhub/Swiftbara Hub", function()
         
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripybara/WeirdGunGame-SwiftbaraHub/refs/heads/main/loader.lua"))()
     end},
-    {"🎯 NXPhub/Swiftbara Hub (Версия 3 - Gun Mods)", function()
+    {"🎯 NXPhub/Swiftbara Hub", function()
   
         loadstring(game:HttpGet("https://pastefy.app/CK8H6men/raw", true))()
     end},
-    {"🎯 NXPhub/Swiftbara Hub (Версия 4 - GUI)", function()
+    {"🎯 NXPhub/Swiftbara Hub", function()
         
         loadstring(game:HttpGet("https://pastefy.app/ivxkplV3/raw", true))()
     end}
