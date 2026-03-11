e7912833a63f331d915c08af5793c2e9911a5135
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-11 17:41:22 +0200

    Update Weird.lua

--- Weird.lua
+++ Weird.lua
@@ -1,9 +1,27 @@
 return {
-        {"Russian Soldier (megahack)", function()
-            loadstring(game:HttpGet("https://pastefy.app/AJPoCZcG/raw"))()
-        end},
-        {"Blox Fruit Script 2", function()
-            print("Blox Fruit Script 2 loaded")
-        end}
-    }
-
+    {"🔥 Weird Gun Game: Mega Hub (Aimbot, ESP, Inf Ammo)", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/zY09e2Vs"))()
+    end},
+    {"🧠 IQ Booster (Universal Script)", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/checkurasshole/Script/refs/heads/main/IQ'))();
+    end},
+    {"⚡ Russian Soldier (MegaHack) - Weird Gun Game", function()
+        loadstring(game:HttpGet("https://pastefy.app/AJPoCZcG/raw"))()
+    end},
+    {"🎯 NXPhub/Swiftbara Hub (Версия 1)", function()
+   
+        loadstring(game:HttpGet("https://pastefy.app/HxgKxr4K/raw"))()
+    end},
+    {"🎯 NXPhub/Swiftbara Hub (Версия 2 - Silent Aim)", function()
+        
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Scripybara/WeirdGunGame-SwiftbaraHub/refs/heads/main/loader.lua"))()
+    end},
+    {"🎯 NXPhub/Swiftbara Hub (Версия 3 - Gun Mods)", function()
+  
+        loadstring(game:HttpGet("https://pastefy.app/CK8H6men/raw", true))()
+    end},
+    {"🎯 NXPhub/Swiftbara Hub (Версия 4 - GUI)", function()
+        
+        loadstring(game:HttpGet("https://pastefy.app/ivxkplV3/raw", true))()
+    end}
+}
