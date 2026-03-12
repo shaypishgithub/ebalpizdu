bc3868dbe7a8203dfed2e2b202a5898674d70102
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-12 12:17:18 +0200

    Add new scripts to ViolenceDistrict.lua

---
+++ ViolenceDistrict.lua
@@ -0,0 +1,26 @@
+return {
+    {"Mana Hub OP", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/god124/Violence-District/refs/heads/main/op"))()
+    end},
+    {"Azure Hub 2026", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/azurelw/azurehub/refs/heads/main/loader.lua"))()
+    end},
+    {"Hendar VD", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/collectorhenfi-wq/Hendarscriptt/refs/heads/main/VDBYHENDAR.txt"))()
+    end},
+    {"Solara VD", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/skidma/solarae/refs/heads/main/vd.lua"))()
+    end},
+    {"ANCH Hub NoKey", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/ayamnubchh/Violence-District-Roblox-Script/main/ANCH-Hax.lua"))()
+    end},
+    {"VGXMOD Reborn", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/VGXMODPLAYER68/VGXMOD-REBORN/refs/heads/main/Violence%20District.lua"))()
+    end},
+    {"Vadrifts AutoRound", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjay/Vadrifts/refs/heads/main/ViolenceDistrict"))()
+    end},
+    {"HM Hub Beta", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/hamozdz/HM-HUB-Violence-District-BETA/refs/heads/main/Protected_7912138782446260.lua"))()
+    end}
+}
