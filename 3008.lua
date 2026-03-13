9660041222c9055dbb514505cb9a823607174438
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-13 13:55:46 +0200

    Add multiple hub scripts for SCP3008 game

---
+++ 3008.lua
@@ -0,0 +1,33 @@
+return {
+    {"Neuron Hub [Топ #1 на ScriptBlox 2026]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Yumiara/Python/refs/heads/main/SCP3008.py"))()
+    end},
+
+    {"VoidPath Hub [free GUI]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/voidpathhub/VoidPath/refs/heads/main/VoidPath.luau"))()
+    end},
+
+    {"Sky Hub [Mobile + ESP]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/yofriendfromschool1/Sky-Hub/main/SkyHub.txt"))()
+    end},
+
+    {"502 Op Gui [Function]", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/502Development/502Lua/main/games/3008.lua'))()
+    end},
+
+    {"Robanik Hub [IKEA style]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Robanik/3008_BY_TOBANIK/refs/heads/main/ROBANIK_3008"))()
+    end},
+
+    {"Niburu TP + ESP Mods", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Niburu52/hub/refs/heads/main/3008%20%5B2.73%5D'))()
+    end},
+
+    {"RealForLife Open Source", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/uRHHZqTr"))()
+    end},
+
+    {"Illya Admin + Worker ESP", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/illyamarj/HUB/refs/heads/main/3008(1).txt'))()
+    end}
+}
