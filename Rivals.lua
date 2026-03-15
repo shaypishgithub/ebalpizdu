550813adcb201598cc23379e76078c1bd5eddef9
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-15 09:29:14 +0200

    Create Rivals.lua

---
+++ Rivals.lua
@@ -0,0 +1,61 @@
+return {
+    {"Z3US Hub [No Key] Aimbot + Silent + ESP + Skin Changer + Rage", function()
+        loadstring(game:HttpGet("https://pastefy.app/YiGY38uo/raw"))()
+    end},
+
+    {"Flow Hub [NEW] Aimbot + RageBot V3 + Unlock All + Safe Rank", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/tffyi0uA"))()
+    end},
+
+    {"Axioma Team Hub [Gun Mods + ESP + Aimbot]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/devKing22/Skidded-ycyberx22/refs/heads/main/rivals"))()
+    end},
+
+    {"Vylera Hub [Visuals + ESP + No Ban]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/vylerascripts/vylera-scripts/main/vylerarivals.lua"))()
+    end},
+
+    {"Xera PBF [Mobile Optimized] Aimbot + ESP + Speed", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/luascriptsROBLOX/Xerar/refs/heads/main/RivalsxeraPBF"))()
+    end},
+
+    {"Minimal Hub [Keyless] Aimbot + ESP + Mobile", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/MinimalScriptingService/MinimalRivals/main/rivals.lua", true))()
+    end},
+
+    {"Rat.dev Loader [Full Pack] Rage + Fly + Unlock", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Kona3366/Rat.dev-Repository/main/loads"))()
+    end},
+
+    {"Soluna [Keyless] TriggerBot + ESP + Charms", function()
+        loadstring(game:HttpGet('https://soluna-script.vercel.app/main.lua', true))()
+    end},
+
+    {"Swift No-Key [Ragebot + Silent Aim + No Smoke]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/awakeningswifters/verycool/refs/heads/main/rvl.lua"))()
+    end},
+
+    {"Simple No-GUI Aimbot + ESP", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/zWhb1mMS"))()
+    end},
+
+    {"Auto-Snap + ESP + Aimbot (No Key)", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/JkXpLdaE"))()
+    end},
+
+    {"KiciaHook Loader [TriggerBot + ESP + Anti-Ban]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/kiciahook/kiciahook/refs/heads/main/loader.lua"))()
+    end},
+
+    {"Tbao Hub [Mobile + PC] Silent Aim + Auto Win + Fly", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/tbao143/thaibao/main/TbaoHubRivals"))()
+    end},
+
+    {"Rybo Aimbot [Best Legit Aimbot + Trigger]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/rybowe/rybowescripts/main/release.lua"))()
+    end},
+
+    {"BlackOwl ZYPHERION [Rage + Anti-Aim]", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/blackowl1231/ZYPHERION/refs/heads/main/main.lua"))()
+    end}
+}
