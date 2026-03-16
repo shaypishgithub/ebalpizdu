ebada45f53229313e1e0ed09ad27bd143d581792
amethyst.qq <125355796+shaypishgithub@users.noreply.github.com>
2026-03-16 09:48:32 +0200

    Update FORSAKEN.lua

--- FORSAKEN.lua
+++ FORSAKEN.lua
@@ -1,1 +1,61 @@
+return {
+    {"Catsaken [Keyless]", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/aibabylaugh/catsaken-real-script-not-assets/refs/heads/main/obfuscated-1448974601077002340.lua'))()
+    end},
 
+    {"Ringta Hub", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/wefwef127382/forsakenloader.github.io/refs/heads/main/RINGTABUBLIK.lua"))()
+    end},
+
+    {"Cursedsaken", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/zxcursedsocute/Forsaken-Script/refs/heads/main/lua"))()
+    end},
+
+    {"Fr0stByt3Z", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Fr0stByt3Z/Fr0stByt3Z-Hub/refs/heads/main/Forsaken%20Obfuscated.lua", true))()
+    end},
+
+    {"Void Ware", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/voidsaken-script/Voidsaken-Loader/refs/heads/main/main"))()
+    end},
+
+    {"Saryn Hub", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Saiky988/Saryn-Hub/refs/heads/main/Saryn.lua'))()
+    end},
+
+    {"Frostware V2", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Actualmrp/FW-Forsaken-V2.0-Rep/refs/heads/main/FrostwareForsakenV2.0Source.txt"))()
+    end},
+
+    {"Funny Hub V2", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/PlutomasterAccount/Funny-Hub-V2-Forsaken/refs/heads/main/Funny%20Hub%20V2%20Forsaken.lua"))()
+    end},
+
+    {"SNT Hub", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Snowt69/SNT-HUB/refs/heads/main/Forsaken"))()
+    end},
+
+    {"AshLab", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/Xan01DmF", true))()
+    end},
+
+    {"Hutao Hub", function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/M1JTpck1"))()
+    end},
+
+    {"SpaceHub", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/ago106/SpaceHub/refs/heads/main/loader.lua'))()
+    end},
+
+    {"c00l GUI V1", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/Altairkaaa/c00l/refs/heads/main/c00lguiv1.lua'))()
+    end},
+
+    {"Rax AutoFarm", function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/raxscripts/LuaUscripts/refs/heads/main/ForsakenAutoFarm.lua"))()
+    end},
+
+    {"Junkie Best", function()
+        loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/b803c8ca9d0205a7561d9f0467418e00b7743ef568f5471653432762027cf0f0/download"))()
+    end}
+}
